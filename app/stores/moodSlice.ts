9e81d83646d9c04e1e681697c2b68df65791c054
emilyb93 <emilybennett93@hotmail.com>
2025-03-06 19:46:32 +0000

    refactor: changes to mood slice state to make bar calculation easier

--- app/stores/moodSlice.ts
+++ app/stores/moodSlice.ts
@@ -2,12 +2,22 @@ import { createSlice } from "@reduxjs/toolkit";
 
 type ValidColour = "red" | "orange" | "yellow" | "green" | "aquamarine";
 type ValidNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+type ColourCount = {
+  [K in ValidColour]: number;
+};
+const initialColourCount = {
+  red: 0,
+  orange: 0,
+  yellow: 0,
+  green: 0,
+  aquamarine: 0,
+};
 // Initial state
 const initialState: {
-  [key: string]: ValidColour[];
+  [key: string]: ColourCount;
 } = {};
 
-function getColour(number: number) {
+function getColour(number: ValidNumber) {
   if (number === 1 || number === 2) return "red";
   if (number === 3 || number === 4) return "orange";
   if (number === 5 || number === 6) return "yellow";
@@ -26,9 +36,12 @@ const moodSlice = createSlice({
 
       feelings.forEach((feeling: string) => {
         if (!state[feeling]) {
-          state[feeling] = [];
+          state[feeling] = { ...initialColourCount };
         }
-        state[feeling].push(colour as ValidColour);
+
+        if (colour) {
+          state[feeling][colour]++;
+        }
       });
       console.log(state);
     },
